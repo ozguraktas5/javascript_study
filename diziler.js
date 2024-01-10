21c961afc8e56c21139a131e399b51dae3770580
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-10 18:58:17 +0300

    10.01.2024

---
+++ diziler.js
@@ -0,0 +1,204 @@
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100];
+
+// const fruits = ["banana", "orange", "mango", "lemon"];
+
+// const vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"];
+
+// const animalProducts = ["milk", "meat", "butter", "yoghurt"];
+
+// const webTechs = ["HTML", "CSS", "JS", "React", "Redux", "Node", "MongDB"];
+
+// const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+
+// console.log("Numbers:", numbers);
+
+// console.log("Number of numbers:", numbers.length);
+
+// console.log("Fruits:", fruits);
+
+// console.log("Number of fruits:", fruits.length);
+
+// console.log("Vegetables:", vegetables);
+
+// console.log("Number of vegetables:", vegetables.length);
+
+// console.log("Animal products:", animalProducts);
+
+// console.log("Number of animal products:", animalProducts.length);
+
+// console.log("Web technologies:", webTechs);
+
+// console.log("Number of web technologies:", webTechs.length);
+
+// console.log("Countries:", countries);
+
+// console.log("Number of countries:", countries.length);
+
+// const arr = [
+//   "Asabeneh",
+
+//   250,
+
+//   true,
+
+//   { country: "Finland", city: "Helsinki" },
+
+//   { skills: ["HTML", "CSS", "JS", "React", "Python"] },
+// ];
+
+// console.log(arr);
+
+// let js = "JavaScript";
+
+// const charsInJavaScript = js.split("");
+
+// console.log(charsInJavaScript);
+
+// let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+// const companies = companiesString.split(",");
+
+// console.log(companies);
+
+// let txt =
+//   "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+
+// const words = txt.split(" ");
+
+// console.log(words);
+
+// const arr = Array()
+
+// console.log(arr)
+
+// const eightEmptyValues = Array(8)
+
+// console.log(eightEmptyValues)
+
+// const eightXValues = Array(8).fill('X')
+
+// console.log(eightXValues)
+
+// const eightOValues = Array(8).fill('O')
+
+// console.log(eightOValues)
+
+// const firstList = [1, 2, 3]
+
+// const secondList = [4, 5, 6]
+
+// const thirdList = firstList.concat(secondList)
+
+// console.log(thirdList)
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.indexOf(5))
+// console.log(numbers.indexOf(3))
+// console.log(numbers.indexOf(0))
+
+// const fruits = ['banana', 'orange', 'mango', 'lemon']
+
+// let index = fruits.indexOf('banana')
+
+// if(index === -1){
+
+// console.log('This fruit does not exist in the array')
+
+// } else {
+
+// console.log('This fruit does exist in the array')
+
+// }
+
+// index === -1 ? console.log('This fruit does not exist in the array') : console.log('This fruit does exist in the array')
+
+// const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+// console.log(numbers.lastIndexOf(2))
+// console.log(numbers.lastIndexOf(1))
+// console.log(numbers.lastIndexOf(4))
+// console.log(numbers.lastIndexOf(6))
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.includes(5))
+// console.log(numbers.includes(0))
+// console.log(numbers.includes(1))
+// console.log(numbers.includes(6))
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers.toString())
+
+// const names = ["Ozgur", "Ebru", "Alev", "Ali"]
+// console.log(names.toString())
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers.join())
+
+// const names = ["Ozgur", "Ebru", "Alev", "Ali"]
+// console.log(names.join(''))
+// console.log(names.join(' # '))
+
+// const  numbers = [1,2,3,4,5]
+
+// console.log(numbers.slice(1,3))
+
+// const arr = ['item1', 'item2', 'item3']
+
+// arr.push('new_item')
+
+// console.log(arr)
+
+// arr.pop()
+
+// console.log(arr)
+
+// arr.shift()
+
+// console.log(arr)
+
+// arr.unshift('item0')
+
+// console.log(arr)
+
+// arr.reverse()
+
+// console.log(arr)
+
+// const  webTechs = [
+
+// 'HTML',
+
+// 'CSS',
+
+// 'JavaScript',
+
+// 'React',
+
+// 'Redux',
+
+// 'Node',
+
+// 'MongoDB'
+
+// ]
+
+// webTechs.sort()
+
+// console.log(webTechs)
+
+const arrayOfArray = [[1, 2, 3], [4, 5, 6]]
+console.log(arrayOfArray[1])
+
+const  frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+
+const  backEnd = ['Node','Express', 'MongoDB']
+
+const fullStack = [frontEnd, backEnd]
+
+console.log(fullStack)
+
+console.log(fullStack.length)
+
+
