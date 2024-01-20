f2b85cce376d11d65a682b4305ed9004b4599f93
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-20 18:47:24 +0300

    20.01.2024

---
+++ scope.js
@@ -0,0 +1,66 @@
+// a = 'Javascript'
+// b = 10
+
+// function letLearnScope() {
+//     console.log(a,b)
+//     if (true) {
+//         console.log(a,b)
+//     }
+// }
+
+// letLearnScope()
+// console.log(a,b)
+
+// let a = "Javascript"
+// let b = 10
+
+// function letsLearnScope() {
+//     console.log(a,b)
+//     if (true) {
+//         let a = "Python"
+//         let b = 20
+//         console.log(a,b)
+//     }
+//     console.log(a,b)
+// }
+
+// letsLearnScope()
+// console.log(a,b)
+
+// let a = "Javascript"
+// let b = 10
+
+// function letsLearnScope() {
+//     console.log(a,b)
+//     let value = false
+//     if (true) {
+//         let a = "Python"
+//         let b = 20
+//         let c = 30
+//         let d = 40
+//         value = !value
+//         console.log(a,b,c,value)
+//     }
+//     console.log(a,b,value)
+// }
+
+// letsLearnScope()
+// console.log(a,b)
+
+
+function letsLearnScope() {
+    var gravity = 9.81
+    console.log(gravity)
+}
+console.log(gravity)
+
+if (true) {
+    var gravity = 9.81
+    console.log(gravity)
+}
+console.log(gravity)
+
+for(var i = 0; i < 3; i++) {
+    console.log(i)
+}
+console.log(i)
