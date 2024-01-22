91f68128f1a6698f5340f256ceaab3ae8695d64a
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-22 11:55:41 +0300

    22.01.2024

--- nesne.js
+++ nesne.js
@@ -22,3 +22,53 @@ console.log(person.lastName)
 
 console.log(person['age'])
 console.log(person["phone number"])
+
+console.log(person.getFullName)
+
+person.nationality = 'German'
+person.country = 'Germany'
+person.title = 'Developer'
+person.skills.push('Meteor')
+person.skills.push('Sass')
+person.isMarried = false
+
+person.getPersonInfo = function() {
+    let skillsWithoutLastSkill = this.skills.splice(0, this.skills.length - 1).join(', ')
+    let lastSkill = this.skills.splice(this.skills.length - 1)[0]
+    let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
+    let fullName = this.getFullName()
+    let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}`
+    return statement
+}
+console.log(person)
+console.log(person.getPersonInfo())
+
+const person2 = {
+    firstName: 'Hale',
+    age: 26,
+    country: 'Italy',
+    city: 'Rome',
+    skills: ['Svelte','Go'],
+    title: 'lawyer',
+    address: {
+        street: 'Trastevere',
+        pobox: 2017,
+        city: 'Rome'
+    },
+    getPersonInfo: function() {
+        return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+    }
+}
+
+const copyPerson = Object.assign({}, person2)
+console.log(copyPerson)
+
+person2.country = "France"
+console.log(person2)
+
+const keys = Object.keys(copyPerson)
+console.log(keys)
+
+const values = Object.values(copyPerson)
+console.log(values)
+
