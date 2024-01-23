07a4d2b4a9d8ff6d05358675dc9aba763f91c40f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-23 19:34:00 +0300

    23.01.2024

--- yuksekderecefonksiyon.js
+++ yuksekderecefonksiyon.js
@@ -19,3 +19,48 @@ const higherOrder = n => {
 }
 
 console.log(higherOrder(5)(6)(7))
+
+const numbers = [1, 2, 3, 4, 5]
+const sumArray = arr => {
+    let sum = 0
+    const callback = function(element) {
+        sum += element
+
+    }
+    arr.forEach(callback)
+    return sum
+}
+
+console.log(sumArray(numbers))
+
+function sayHello() {
+    console.log("Hello!")
+}
+
+//setInterval(sayHello, 1000)
+
+function sayHello2() {
+    console.log("Hello2!")
+}
+
+//setTimeout(sayHello2, 3000)
+
+// arr.forEach(function (element, index, arr) {
+//     console.log(element, index, arr)
+// })
+
+// arr.forEach((element, index, arr) => {
+//     console.log(element, index, arr)
+// })
+
+// arr.forEach((element, index, arr) => console.log(element, index, arr))
+
+let sum = 0
+const numbers2 = [1,2,3,4,5]
+numbers2.forEach(number => console.log(number))
+
+numbers2.forEach(number => sum += number)
+console.log(sum)
+
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+countries.forEach(country => console.log(country.toUpperCase()))
