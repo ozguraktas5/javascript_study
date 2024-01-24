4bbb000698d94ef8449f86ec33e6c14692b1fbbb
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-24 18:18:29 +0300

    24.01.2024

---
+++ 9dayshomework.js
@@ -0,0 +1,34 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+const callback = (x) => {
+    return x ** 3
+}
+
+console.log(callback(3))
+
+countries.forEach((item) => console.log(item))
+
+names.forEach((name) => console.log(name))
+
+numbers.forEach((number) => console.log(number))
+
+const countryUpper = countries.map((country) => country.toUpperCase())
+console.log(countryUpper)
+
+const countryLengthSeven = countries.map((country) => country.length)
+console.log(countryLengthSeven)
+
+const numbersSquare = numbers.map((item) => {
+    return item ** 2
+})
+console.log(numbersSquare)
