720daa36c2cf5737f47c35fd3ae9e4e56d496dc4
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-25 15:25:53 +0300

    25.01.2024

--- setsandmaps.js
+++ setsandmaps.js
@@ -40,3 +40,27 @@ for (const companies of companies3) {
 }
 
 console.log(companiesSet)
+
+console.log(companiesSet.delete('Amazon'))
+console.log(companiesSet)
+
+console.log(companiesSet.has('Oracle'))
+console.log(companiesSet.has('Monster'))
+
+companiesSet.clear()
+console.log(companiesSet)
+
+const langSet = new Set(languages)
+console.log(langSet)
+console.log(langSet.size)
+
+const counts = []
+const count = {}
+
+for(const l of langSet) {
+    const filteredLang = languages.filter((lng) => lng === l)
+    console.log(filteredLang)
+    counts.push({ lang: l, count: filteredLang.length })
+}
+
+console.log(counts)
