4cc752d073517238618f0932e9c54a34e005d2aa
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-26 13:02:47 +0300

    26.01.2024

--- 10dayshomework.js
+++ 10dayshomework.js
@@ -66,3 +66,25 @@ for (const language of countriesDataLength) {
 }
 
 console.log(sum)
+
+const languages =  [
+     { English: 91 },
+     { French: 45 },
+     { Arabic: 25 },
+     { Spanish: 24 },
+     { Russian: 9 },
+     { Portuguese: 9 },
+     { Dutch: 8 },
+     { German: 7 },
+     { Chinese: 5 },
+     { Swahili: 4 },
+     { Serbian: 4 }
+   ]
+
+function mostSpokenLanguages(languages, count) {
+    const languagesSort = languages.sort((x, y) => y - x)
+    const languagesCount = languagesSort.slice(0, count)
+    return languagesCount
+}
+
+console.log(mostSpokenLanguages(languages, 3))
