26fd5d2aabbd64bdae7e80cc65ec3459868aca2f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-29 14:59:30 +0300

    29.01.2024

--- regularexpressions.js
+++ regularexpressions.js
@@ -20,3 +20,49 @@ const pattern3 = /love/
 const result3 = pattern3.test(str3)
 
 console.log(result3)
+
+const str4 = 'I love Javascript'
+const pattern4 = /love/
+const result4 = str4.match(pattern4)
+
+console.log(result4)
+
+const str5 = 'I love Javascript'
+const pattern5 = /love/g
+const result5 = str5.match(pattern5)
+
+console.log(result5)
+
+const str6 = 'I love Javascript'
+const pattern6 = /love/g
+const result6 = str6.search(pattern6)
+
+console.log(result6)
+
+const txt = 'Python is the most beautiful language that a human begin has ever created.\
+I recommend python for a first programming language'
+matchReplaced = txt.replace(/Python|python/, 'JavaScript')
+
+console.log(matchReplaced)
+
+const txt2 = 'Python is the most beautiful language that a human begin has ever created.\
+I recommend python for a first programming language'
+matchReplaced = txt.replace(/Python|python/g, 'JavaScript')
+
+console.log(matchReplaced)
+
+const txt3 = 'Python is the most beautiful language that a human begin has ever created.\
+I recommend python for a first programming language'
+matchReplaced = txt.replace(/Python/gi, 'JavaScript')
+
+console.log(matchReplaced)
+
+const txt4 = '%I a%m te%%a%%che%r% a%n%d %% I l%o%ve te%ach%ing.\
+T%he%re i%s n%o%th%ing as m%ore r%ewarding a%s e%duc%at%i%ng a%n%d e%m%p%ow%er%ing \
+p%e%o%ple.\
+I fo%und te%a%ching m%ore i%n%t%er%%es%ting t%h%an any other %jobs.\
+D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher.'
+matches = txt4.replace(/%/g, '')
+
+console.log(matches)
+
