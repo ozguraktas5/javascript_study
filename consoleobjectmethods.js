72b394c26401d63c215ef563942d43ca9b772019
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-31 20:37:41 +0300

    31.01.2024

--- consoleobjectmethods.js
+++ consoleobjectmethods.js
@@ -12,3 +12,36 @@ console.log(
   "",
   "color:yellow"
 );
+
+console.warn('This is a warning')
+
+console.warn(
+  'You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!'
+)
+
+console.warn('Warning is different from error')
+
+console.error('This is an error message')
+
+console.error('We all make mistakes')
+
+const names = ['Ozgur', 'Ayse', 'Veli', 'Ali']
+console.table(names)
+
+const user = {
+    name: 'Ozgur',
+    title: 'Developer',
+    country: 'Turkey',
+    city: 'Samsun',
+    age: 32
+}
+
+console.table(user)
+
+const countries = [
+    ['Turkey',  'Istanbul'],
+    ['Germany', 'Berlin'],
+    ['England', 'London']
+]
+
+console.table(countries)
