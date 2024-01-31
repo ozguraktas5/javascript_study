6577d94d8ecc47e83f23dcbcb6ee2e1641dfc4f8
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-31 20:43:25 +0300

    31.01.2024

--- consoleobjectmethods.js
+++ consoleobjectmethods.js
@@ -13,35 +13,91 @@ console.log(
   "color:yellow"
 );
 
-console.warn('This is a warning')
+console.warn("This is a warning");
 
 console.warn(
-  'You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!'
-)
+  "You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!"
+);
 
-console.warn('Warning is different from error')
+console.warn("Warning is different from error");
 
-console.error('This is an error message')
+console.error("This is an error message");
 
-console.error('We all make mistakes')
+console.error("We all make mistakes");
 
-const names = ['Ozgur', 'Ayse', 'Veli', 'Ali']
-console.table(names)
+const names = ["Ozgur", "Ayse", "Veli", "Ali"];
+console.table(names);
 
 const user = {
-    name: 'Ozgur',
-    title: 'Developer',
-    country: 'Turkey',
-    city: 'Samsun',
-    age: 32
-}
+  name: "Ozgur",
+  title: "Developer",
+  country: "Turkey",
+  city: "Samsun",
+  age: 32,
+};
 
-console.table(user)
+console.table(user);
 
 const countries = [
-    ['Turkey',  'Istanbul'],
-    ['Germany', 'Berlin'],
-    ['England', 'London']
-]
+  ["Turkey", "Istanbul"],
+  ["Germany", "Berlin"],
+  ["England", "London"],
+];
 
-console.table(countries)
+console.table(countries);
+
+const users = [
+  {
+    name: "Asabeneh",
+    title: "Programmer",
+    country: "Finland",
+    city: "Helsinki",
+    age: 250,
+  },
+  {
+    name: "Eyob",
+    title: "Teacher",
+    country: "Sweden",
+    city: "London",
+    age: 25,
+  },
+  {
+    name: "Asab",
+    title: "Instructor",
+    country: "Norway",
+    city: "Oslo",
+    age: 22,
+  },
+  {
+    name: "Matias",
+    title: "Developer",
+    country: "Denmark",
+    city: "Copenhagen",
+    age: 28,
+  },
+];
+console.table(users);
+
+const countries2 = [
+  ["Turkey", "Istanbul"],
+  ["Germany", "Berlin"],
+  ["England", "London"],
+];
+
+console.time("Regular for loop");
+for (let i = 0; i < countries2.length; i++) {
+  console.log(countries2[i][0], countries2[i][1]);
+}
+console.timeEnd("Regular for loop");
+
+console.time("for of loop");
+for (const [name, city] of countries) {
+  console.log(name, city);
+}
+console.timeEnd("for of loop");
+
+console.time("forEach loop");
+countries.forEach(([name, city]) => {
+  console.log(name, city);
+});
+console.timeEnd("forEach loop");
