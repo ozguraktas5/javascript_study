cfb287d190e4b983b45c3c98d2397e8b9a341f37
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-01-31 19:54:08 +0300

    31.01.2024

---
+++ consoleobjectmethods.js
@@ -0,0 +1,14 @@
+console.log("30 Days of Javascript");
+
+console.log("%d %s of JavaScript", 30, "Days");
+
+console.log("%c30 Days Of JavaScript", "color:green ");
+
+console.log(
+  "%c30 Days%c %cOf%c %cJavaScript",
+  "color:green",
+  "",
+  "color:red",
+  "",
+  "color:yellow"
+);
