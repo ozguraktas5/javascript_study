a4b2cdc58351363a22c91c4d27b2a3c8f4cb3897
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-04 13:45:16 +0300

    04.02.2024

--- consoleobjectmethods.js
+++ consoleobjectmethods.js
@@ -114,3 +114,61 @@ for (let i = 0; i <= 10; i += 1) {
   console.log('the # is ' + i)
   console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage })
 }
+
+const names3 = ["Ahmet", "Mehmet", "Zeynep", "Ali"]
+const countries3 = [
+  ["Turkey", "Istanbul"],
+  ["Germany", "Berlin"],
+  ["England", "London"],
+]
+const user3 = {
+  name: "Ahmet",
+  title: "Programmer",
+  country: "Turkey",
+  city: "Istanbul",
+  age: 27
+}
+const users3 = [
+  {
+    name: "Ahmet",
+    title: "Programmer",
+    country: "Turkey",
+    city: "Istanbul",
+    age: 27
+  },
+  {
+    name: "Mehmet",
+    title: "Teacher",
+    country: "Turkey",
+    city: "Ankara",
+    age: 28
+  },
+  {
+    name: "Zeynep",
+    title: "Instructor",
+    country: "Turkey",
+    city: "Izmir",
+    age: 26
+  },
+  {
+    name: "Ali",
+    title: "Engineer",
+    country: "Turkey",
+    city: "Samsun",
+    age: 22
+  }
+]
+
+console.group('Names')
+console.log(names3)
+console.groupEnd()
+
+console.group('Countries')
+console.log(countries3)
+console.groupEnd()
+
+console.group('Users')
+console.log(user3)
+console.log(users3)
+console.groupEnd()
+
