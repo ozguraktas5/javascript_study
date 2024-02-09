69de7e8ecf0c9412cc415e60fab8193f89d444fe
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-09 20:27:22 +0300

    09.02.2024

--- 18dayshomework.js
+++ 18dayshomework.js
@@ -1,4 +1,4 @@
-// const urlCountries = 'https://restcountries.com/v2/all';
+const urlCountries = "https://restcountries.com/v2/all";
 // fetch(urlCountries)
 //     .then(response => response.json())
 //     .then(data => {
@@ -22,15 +22,44 @@
 //     })
 //     .catch(err => console.log(err))
 
-const catAverageWeight = 'https://api.thecatapi.com/v1/breeds';
-fetch(catAverageWeight)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(cat => {
-            if (cat.weight.metric.match(/\d+/g)) {
-                console.log(cat.weight.metric)
-            }
-        })
-    })
-    .catch(err => console.log(err))
+// const catAverageWeight = 'https://api.thecatapi.com/v1/breeds';
+// fetch(catAverageWeight)
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(cat => {
+//             if (cat.weight.metric.match(/\d+/g)) {
+//                 console.log(cat.weight.metric)
+//             }
+//         })
+//     })
+//     .catch(err => console.log(err))
 
+// fetch(urlCountries)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data
+//       .sort((a, b) => b.area - a.area)
+//       .slice(0, 10)
+//       .forEach((country) => {
+//         console.log(country.name);
+//         console.log(country.area);
+//       });
+//   })
+//   .catch((err) => console.log(err));
+
+fetch(urlCountries)
+  .then((response) => response.json())
+  .then(data => {
+    const totalLanguages = [];
+    data.forEach(country => {
+        if (country.languages && country.languages.length > 0) {
+            country.languages.forEach(language => {
+                if (!totalLanguages.includes(language.name)) {
+                    totalLanguages.push(language.name);
+                }
+            })
+        }
+    })
+    console.log(totalLanguages.length)
+  })
+  .catch((err) => console.log(err));
