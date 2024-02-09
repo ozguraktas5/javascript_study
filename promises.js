7faf32dbeb5a7bae10e373f7e0aaefd9347be3a2
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-09 14:38:27 +0300

    09.02.2024

--- promises.js
+++ promises.js
@@ -27,4 +27,38 @@ doSomething2((error, result) => {
         return console.log(error)
     }
     return console.log(result)
-}) 
+})
+
+const doPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const skills = ['HTML', 'CSS', 'JS']
+        if (skills.length > 0) {
+            resolve(skills)
+        } else {
+            reject('Something wrong has happened')
+        }
+    }, 2000)
+})
+
+doPromise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.log(error))
+
+const doPromise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const skills = ['HTML', 'CSS', 'JS']
+        if (skills.includes('Node')) {
+            resolve('fullstack developer')
+        } else {
+            reject('Something wrong has happened')
+        }
+    }, 2000)
+})
+
+doPromise2
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.log(error))
