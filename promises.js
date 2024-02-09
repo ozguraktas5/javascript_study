97f01757db8a0706e5bcf171f333ed778a1b3e54
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-09 15:19:54 +0300

    09.02.2024

--- promises.js
+++ promises.js
@@ -62,3 +62,39 @@ doPromise2
         console.log(result)
     })
     .catch(error => console.log(error))
+
+const url = 'https://restcountries.com/v2/all'
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+const square = async function (n) {
+    return n * n
+}
+
+const calculateSquare = async function() {
+    const value = await square(2)
+    console.log(value)
+}
+
+calculateSquare()
+
+const url2 = 'https://restcountries.com/v2/all'
+fetch(url2)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+const fetchData = async () => {
+    try {
+        const response = await fetch(url)
+        const countries = await response.json()
+        console.log(countries)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+console.log('===== async and await')
+fetchData()
