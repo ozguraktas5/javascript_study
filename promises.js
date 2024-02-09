9cbeca823df26aff0f166022ee3a453a9925c45c
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-09 14:04:53 +0300

    09.02.2024

--- promises.js
+++ promises.js
@@ -14,3 +14,17 @@ const callback = (error, result) => {
 }
 
 doSomething(callback)
+
+const doSomething2 = callback => {
+    setTimeout(() => {
+        const skills = ['HTML', 'CSS', 'JS']
+        callback(false, skills)
+    }, 2000)
+}
+
+doSomething2((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+    return console.log(result)
+}) 
