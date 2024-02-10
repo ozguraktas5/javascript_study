2e0a185cd27bfa0aaec27c2d1beda4cc22bc521f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-10 11:37:28 +0300

    10.02.2024

--- closures.js
+++ closures.js
@@ -13,3 +13,23 @@ console.log(innerFunc())
 console.log(innerFunc())
 console.log(innerFunc())
 
+function outerFunction2() {
+    let count = 0
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne: plusOne(),
+        minusOne: minusOne()
+    }
+}
+
+const innerFuncs = outerFunction2()
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
