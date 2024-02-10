78109671b3399dc41b74ce47c49f94bb4dc7e8b9
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-10 20:12:13 +0300

    10.02.2024

--- writingcleancodes.js
+++ writingcleancodes.js
@@ -76,7 +76,69 @@ const person = {
 
 }
 
-for(const value in person){
-    console.log(person[value])
+for(const key in person){
+    console.log(key)
+}
+
+const person2 = {
+    firstName: 'Gokhan',
+    lastName: 'Kuscu',
+    age: 42,
+    country: 'Turkey',
+    city: 'Bursa',
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python'],
+    isMarried: true
+
+}
+
+for(const key in person2){
+    console.log(key, person2[key])
+}
+
+let num = 3
+if (num > 0) {
+    console.log(`${num} is a positive number`)
+} else {
+    console.log(`${num} is a negative number`)
+}
+
+let a = 0
+if (a > 0) {
+    console.log(`${a} is a positive number`)
+} else if (a < 0) {
+    console.log(`${a} is a negative number`)
+} else if (a == 0) {
+    console.log(`${a} is zero`)
+} else {
+    console.log(`${a} is not a number`)
+}
+
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch(day) {
+    case 'monday':
+        console.log('Today is Monday')
+        break
+    case 'tuesday':
+        console.log('Today is Tuesday')
+        break
+    case 'wednesday':
+        console.log('Today is Wednesday')
+        break
+    case 'thursday':
+        console.log('Today is Thursday')
+        break
+    case 'friday':
+        console.log('Today is Friday')
+        break
+    case 'saturday':
+        console.log('Today is Saturday')
+        break
+    case 'sunday':
+        console.log('Today is Sunday')
+        break
+    default:
+        console.log('It is not a week day.')
 }
 
