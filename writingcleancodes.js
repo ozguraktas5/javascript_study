09ef29c06465d39b25e3c11ee29a2cfd291360ff
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-10 20:15:07 +0300

    10.02.2024

--- writingcleancodes.js
+++ writingcleancodes.js
@@ -113,32 +113,44 @@ if (a > 0) {
     console.log(`${a} is not a number`)
 }
 
-let dayUserInput = prompt('What day is today ?')
-let day = dayUserInput.toLowerCase()
+// let dayUserInput = prompt('What day is today ?')
+// let day = dayUserInput.toLowerCase()
 
-switch(day) {
-    case 'monday':
-        console.log('Today is Monday')
-        break
-    case 'tuesday':
-        console.log('Today is Tuesday')
-        break
-    case 'wednesday':
-        console.log('Today is Wednesday')
-        break
-    case 'thursday':
-        console.log('Today is Thursday')
-        break
-    case 'friday':
-        console.log('Today is Friday')
-        break
-    case 'saturday':
-        console.log('Today is Saturday')
-        break
-    case 'sunday':
-        console.log('Today is Sunday')
-        break
-    default:
-        console.log('It is not a week day.')
+// switch(day) {
+//     case 'monday':
+//         console.log('Today is Monday')
+//         break
+//     case 'tuesday':
+//         console.log('Today is Tuesday')
+//         break
+//     case 'wednesday':
+//         console.log('Today is Wednesday')
+//         break
+//     case 'thursday':
+//         console.log('Today is Thursday')
+//         break
+//     case 'friday':
+//         console.log('Today is Friday')
+//         break
+//     case 'saturday':
+//         console.log('Today is Saturday')
+//         break
+//     case 'sunday':
+//         console.log('Today is Sunday')
+//         break
+//     default:
+//         console.log('It is not a week day.')
+// }
+
+let isRaining = true
+isRaining
+    ? console.log('You need a rain coat.')
+    : console.log('No need for a rain coat.')
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
 }
 
