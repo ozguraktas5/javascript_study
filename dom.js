12057554742dda79ec1c52aab60b383319e8e730
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-12 13:43:51 +0300

    12.02.2024

--- dom.js
+++ dom.js
@@ -15,3 +15,23 @@ console.log(allTitles2.length)
 for (let i = 0; i < allTitles2.length; i++) {
     console.log(allTitles2[i])
 }
+
+let firstTitle = document.getElementById('first-title')
+console.log(firstTitle)
+
+let firstTitle2 = document.querySelector('h1')
+let firstTitle3 = document.querySelector('#first-title')
+let firstTitle4 = document.querySelector('.title')
+
+const allTitles3 = document.querySelectorAll('h1')
+
+console.log(allTitles3.length)
+for (let i = 0; i < allTitles3.length; i++) {
+    console.log(allTitles3[i])
+}
+
+allTitles3.forEach(title => console.log(title))
+
+const titles = document.querySelectorAll('h1')
+titles[3].className = 'title'
+titles[3].id = 'fourth-title'
