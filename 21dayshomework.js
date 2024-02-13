d5f3b230a3a758302a7adb409dd50e8fd02502e5
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-13 18:19:23 +0300

    13.02.2024

---
+++ 21dayshomework.js
@@ -0,0 +1,17 @@
+let firstP = document.querySelector("p");
+console.log(firstP)
+
+let firstPe = document.querySelector('#first-p')
+console.log(firstPe)
+
+let secondPe = document.querySelector('#second-p')
+console.log(secondPe)
+
+let thirdPe = document.querySelector('#third-p')
+console.log(thirdPe)
+
+let fourthPe = document.querySelector('#fourth-p')
+console.log(fourthPe)
+
+let allPe = document.querySelectorAll('p')
+console.log(allPe)
